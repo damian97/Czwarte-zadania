6b8d8f67d97606c11200a68c7c32d61d87818935
Sryncu <Sryncu@Sryncu-Komputer>
2017-12-02 09:26:24 +0100

    Zmiany

---
+++ src/Zadanie2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Zadanie2 {
+
+	public static void main(String[] args) {
+	
+		Scanner odczyt = new Scanner(System.in);
+		
+		String ciag = odczyt.nextLine();
+	
+		StringBuilder sb = new StringBuilder().append(ciag).reverse();
+		
+		System.out.println(sb);
+		
+	}
+		
+}//
--- src/Zadanie3.java
+++ src/Zadanie3.java
@@ -37,7 +37,7 @@ public class Zadanie3 {
 		
 		}
 		
-		//dsdsdsdsdsdsdss
+
 		
 		
 		
